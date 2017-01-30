d9314a601913f7cc25e6dce4742545aa3da77135
Daniel Carapia <toonlink4943@gmail.com>
2017-01-30 09:19:41 -0700

    Removed www from all links

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var request = new XMLHttpRequest();
-var genKnowledge = 'https://www.opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple';
-var videoGames = 'https://www.opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
+var genKnowledge = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple';
+var videoGames = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
 var mathematics = 'https://opentdb.com/api.php?amount=10&category=19&difficulty=medium';
 var film = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
-var animals = 'https://www.opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple';
+var animals = 'https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple';
 
 loadData();
 
 function loadData(){
-    request.open('GET', 'https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple');
+    request.open('GET', genKnowledge);
     request.onload = loadComplete;
     request.send();
 }
