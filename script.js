4950738a0463bc3f021c6117601f65a9abd8763b
Adrian Wenceslao <awenceslao@student.neumont.edu>
2017-02-01 09:43:37 -0700

    First commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var request = new XMLHttpRequest();
 var genKnowledge = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple';
 var videoGames = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple';
-var mathematics = 'https://opentdb.com/api.php?amount=10&category=19&difficulty=medium';
+var history = 'https://opentdb.com/api.php?amount=10&category=23&difficulty=medium&type=multiple';
 var film = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
 var animals = 'https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple';
 
@@ -13,10 +13,19 @@ function loadData(){
     request.send();
 }
 
+var questions = [10];
+var answers = [4];
+var questionSet = "";
+var answerSet = ""; 
+
 function loadComplete(evt){
     quizData = JSON.parse(request.responseText);
     for(var i  = 0; i < 10; i++){
-        console.log(quizData.results[i]);
-        
+        questions[i] = quizData.results[i].question;
+        questionSet += "<div>" + questions[i] + "</div>" + "</br>";
+        console.log(quizData.results[i]);   
     }
+    document.getElementById("QuestionDisplay").innerHTML = questionSet;
 }
+
+
