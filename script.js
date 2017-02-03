8ad55442ac3b5e03ea860f4c63f9060947b7d31a
Adrian Wenceslao <awenceslao@student.neumont.edu>
2017-02-03 08:42:19 -0700

    2/3/17 - Adrian

--- script.js
+++ script.js
@@ -61,23 +61,29 @@ function getParameter(){
 }
 
 var questions = [10];
-var response; 
-var questionBegin = "";
-var questionEnd = "</div>" + "</br>"
+var response;
+var questionDis = ""; 
+var answerDis = "";
+// var questionBegin = "";
+// var questionEnd = "</div>" + "</br>";
 
 function loadComplete(evt){
     quizData = JSON.parse(request.responseText);
     for(var i  = 0; i < 10; i++){
         questions[i] = quizData.results[i].question;
         console.log(quizData.results[i]);
-        questionBegin += "<div>" + questions[i] + "</br>" + "</br>" + 
-        quizData.results[i].correct_answer + "</br>" +
-        quizData.results[i].incorrect_answers[0] + "</br>" + 
-        quizData.results[i].incorrect_answers[1] + "</br>" +
-        quizData.results[i].incorrect_answers[2] + "</br"  + "</br>" + 
-        questionEnd; 
+        questionDis += "<div>" + questions[i] + "</br> </br> </div> </br> "
+        answerDis += "<div>" + quizData.results[i].correct_answer + "</div>";
+
+        // questionBegin += "<div>" + questions[i] + "</br>" + "</br>" + 
+        // quizData.results[i].correct_answer + "</br>" +
+        // quizData.results[i].incorrect_answers[0] + "</br>" + 
+        // quizData.results[i].incorrect_answers[1] + "</br>" +
+        // quizData.results[i].incorrect_answers[2] + "</br"  + "</br>" + 
+        // questionEnd; 
     }
-    document.getElementById("QuestionDisplay").innerHTML = questionBegin;
+    document.getElementById("QuestionDisplay").innerHTML = questionDis;
+    document.getElementById("AnswerDisplay").innerHTML = answerDis;
 }
 
 
